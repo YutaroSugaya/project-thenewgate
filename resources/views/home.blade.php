c39cac9dbc4454f0ecb1f407285ae949c0bf65ae
Hikaru Shiho <shiho97797@gmail.com>
2021-12-25 11:58:03 +0900

    2021/12/25

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -25,40 +25,46 @@
                             <span class="text-594838 font-weight-bold text_middle">タカハシ商事</span>
                         </a>
                     </h1>
-                    <div>
-                        <select class="custom-select text-white font-weight-bold bg-a68a72 ml-3 border border-a68a72">
+                    <div class="pl-4">
+                        <select class="custom-select text-white font-weight-bold bg-a68a72 border border-a68a72">
                         <option selected>商品カテゴリー</option>
                         <option value="1">テスト</option>
                         <option value="2">テスト</option>
                         <option value="3">テスト</option>
                         </select>
                     </div>
-                    <div class="ml-4">
-                        <input type="text" placeholder="何をお探しですか？" class="text_normal border border-a68a72">
+                    <div class="pl-4">
+                        <div class="input-group">
+                            <input type="text" class="form-control font-weight-bold border border-a68a72" placeholder="何をお探しですか？" aria-label="Recipient's username" aria-describedby="button-addon2">
+                            <div class="input-group-append">
+                                <button class="btn btn-outline-secondary font-weight-bold bg-a68a72 text-white border border-a68a72" type="button" id="button-addon2">検索</button>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div></div>
+                <div class="d-flex">
+                    <div class="d-flex justify-content-center align-items-center bg-a64444" style="width: 94px; height: 86px;">
+                        <a href="">
+                            <div class="text-center">
+                                <img src="{{ asset('images/common/icon_login.png') }}" alt="" width="21" height="23" class="img-fluid">
+                                <span class="d-block text-white font-weight-bold pt-1">ログイン</span>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="d-flex justify-content-center align-items-center bg-6f2c2c" style="width: 94px; height: 86px;">
+                        <div class="text-center">
+                            <a href="">
+                                <img src="{{ asset('images/common/icon_register.png') }}" alt="" width="21" height="23" class="img-fluid">
+                                <span class="d-block text-white font-weight-bold pt-1">新規登録</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
             </div>
         </header>
 
         <div class="mw-100">
-            <div class="swiper-container">
-                <div class="swiper-wrapper">
-                    <div class="swiper-slide">
-                        <img src="{{ asset('images/home/demo_main01.jpg') }}" alt="" width="100%" height="auto" class="img-fluid">
-                    </div>
-                    <div class="swiper-slide">
-                        <img src="{{ asset('images/home/demo_main01.jpg') }}" alt="" width="100%" height="auto" class="img-fluid">
-                    </div>
-                    <div class="swiper-slide">
-                        <img src="{{ asset('images/home/demo_main01.jpg') }}" alt="" width="100%" height="auto" class="img-fluid">
-                    </div>
-                    ...
-                </div>
-                <div class="swiper-pagination"></div>
-                <div class="swiper-button-prev"></div>
-                <div class="swiper-button-next"></div>
-            </div>
+            <img src="{{ asset('images/home/demo_main01.jpg') }}" alt="" width="100%" height="auto" class="img-fluid">
         </div>
 
 
@@ -259,6 +265,80 @@
                 </div>
             </div>
         </section>
+
+
+        <footer class="bg-594838">
+            <div class="container py-4 py-md-5">
+                <div class="d-md-flex">
+                    <div>
+                        <p>
+                            <span class="text-white text_middle font-weight-bold">タカハシ商事</span>
+                        </p>
+                        <p class="text-white font-weight-bold">
+                            〒123-45678<br>
+                            東京都○○○区○○番地○○号　○○○ビル1階<br>
+                            TEL：123-5678　FAX：123-5678
+                        </p>
+                    </div>
+                    <div class="d-md-flex pl-md-5">
+                        <ul class="list-unstyled pl-md-5 my-0">
+                            <li class="py-2">
+                                <a href="">
+                                    <span class="text-white font-weight-bold">ホーム</span>
+                                </a>
+                            </li>
+                            <li class="py-2">
+                                <a href="">
+                                    <span class="text-white font-weight-bold">ホーム</span>
+                                </a>
+                            </li>
+                            <li class="py-2">
+                                <a href="">
+                                    <span class="text-white font-weight-bold">ホーム</span>
+                                </a>
+                            </li>
+                        </ul>
+                        <ul class="list-unstyled pl-md-5 my-0">
+                            <li class="py-2">
+                                <a href="">
+                                    <span class="text-white font-weight-bold">ホーム</span>
+                                </a>
+                            </li>
+                            <li class="py-2">
+                                <a href="">
+                                    <span class="text-white font-weight-bold">ホーム</span>
+                                </a>
+                            </li>
+                            <li class="py-2">
+                                <a href="">
+                                    <span class="text-white font-weight-bold">ホーム</span>
+                                </a>
+                            </li>
+                        </ul>
+                        <ul class="list-unstyled pl-md-5 my-0">
+                            <li class="py-2">
+                                <a href="">
+                                    <span class="text-white font-weight-bold">ホーム</span>
+                                </a>
+                            </li>
+                            <li class="py-2">
+                                <a href="">
+                                    <span class="text-white font-weight-bold">ホーム</span>
+                                </a>
+                            </li>
+                            <li class="py-2">
+                                <a href="">
+                                    <span class="text-white font-weight-bold">ホーム</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="bg-fff3e8 text_small text-center text-594838 font-weight-bold py-md-2 py-3">
+                Copyright &copy;2016 ○○○○. All rights reserved.
+            </div>
+        </footer>
 
 
         <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.4.1/js/swiper.min.js"></script>
