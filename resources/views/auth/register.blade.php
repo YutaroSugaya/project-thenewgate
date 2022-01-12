7415e2f7bd568a15e47daec1556d72fb077a11ea
Yutaro.Sugaya <tech_nine0400@yahoo.co.jp>
2022-01-12 18:57:56 +0900

    新規登録画面のバリデーションの修正

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,8 +12,7 @@
                             @csrf
 
                             <div class="row mb-4 ">
-                                <label for="name"
-                                    class="col-md-4 col-form-label text-md-right">{{ __('お名前') }}</label>
+                                <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('お名前') }}</label>
 
                                 <div class="col-md-6">
 
@@ -39,8 +38,7 @@
                             </div>
 
                             <div class="row mb-4 ">
-                                <label for="name"
-                                    class="col-md-4 col-form-label text-md-right">{{ __('フリガナ') }}</label>
+                                <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('フリガナ') }}</label>
 
                                 <div class="col-md-6">
 
@@ -78,8 +76,9 @@
                                 <div class="col-md-3">
                                     <input id="post_code" type="text" placeholder="ハイフン不要"
                                         class="form-control @error('post_code') is-invalid @enderror"
-                                        value="{{ old('post_code') }}" required autocomplete="post_code" name="zip01" size="10"
-                                        maxlength="8" onKeyUp="AjaxZip3.zip2addr(this,'','pref01','addr01');">
+                                        value="{{ old('post_code') }}" required autocomplete="post_code" pattern="\d*"
+                                        name="zip01" size="10" maxlength="8"
+                                        onKeyUp="AjaxZip3.zip2addr(this,'','pref01','addr01');">
 
                                     @error('post_code')
                                         <span class="invalid-feedback" role="alert">
@@ -97,7 +96,8 @@
                                     class="col-md-4 col-form-label text-md-right">{{ __('都道府県') }}</label>
 
                                 <div class="col-md-2">
-                                    <input type="text" name="pref01" size="20" id="address"class="form-control @error('address') is-invalid @enderror"
+                                    <input type="text" name="pref01" size="20" id="address"
+                                        class="form-control @error('address') is-invalid @enderror"
                                         value="{{ old('address') }}" required autocomplete="address">
 
                                     @error('address')
@@ -114,8 +114,10 @@
                                     class="col-md-4 col-form-label text-md-right">{{ __('以降の住所') }}</label>
 
                                 <div class="col-md-6">
-                                    <input type="text" name="addr01" size="60" id="address"class="form-control @error('address') is-invalid @enderror"
-                                        value="{{ old('address') }}" required autocomplete="address" placeholder="市区町村、番地、建物名など">
+                                    <input type="text" name="addr01" size="60" id="address"
+                                        class="form-control @error('address') is-invalid @enderror"
+                                        value="{{ old('address') }}" required autocomplete="address"
+                                        placeholder="市区町村、番地、建物名など">
 
                                     @error('address')
                                         <span class="invalid-feedback" role="alert">
@@ -131,8 +133,10 @@
                                     class="col-md-4 col-form-label text-md-right">{{ __('電話番号') }}</label>
 
                                 <div class="col-md-3">
-                                    <input id="phone_number" type="a" class="form-control @error('phone_number') is-invalid @enderror"
-                                        name="phone_number" value="{{ old('phone_number') }}" required autocomplete="phone_number" maxlength="11" placeholder="ハイフン不要">
+                                    <input id="phone_number" type="a"
+                                        class="form-control @error('phone_number') is-invalid @enderror" name="phone_number"
+                                        value="{{ old('phone_number') }}" required autocomplete="phone_number"
+                                        pattern="[0-9]{9,12}" placeholder="ハイフン不要">
 
                                     @error('phone_number')
                                         <span class="invalid-feedback" role="alert">
@@ -167,7 +171,7 @@
                                 <div class="col-md-6">
                                     <input id="password" type="password"
                                         class="form-control @error('password') is-invalid @enderror" name="password"
-                                        required autocomplete="new-password">
+                                        required autocomplete="new-password" pattern="[a-z,A-Z,0-9]{6,20}">
 
                                     @error('password')
                                         <span class="invalid-feedback" role="alert">
@@ -184,8 +188,9 @@
 
                                 <div class="col-md-6">
                                     <input id="password-confirm" type="password" class="form-control"
-                                        name="password_confirmation" required autocomplete="new-password">
-                                        <p>(半角英数字 6〜20文字) ※機種依存文字は使えません。</p>
+                                        name="password_confirmation" required autocomplete="new-password"
+                                        pattern="[a-z,A-Z,0-9]{6,20}">
+                                    <p>(半角英数字 6〜20文字) ※機種依存文字は使えません。</p>
                                 </div>
                             </div>
 
