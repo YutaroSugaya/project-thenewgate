1466ea9c5b41c1b52dbbdfcc990e4c57d9527afc
Yutaro.Sugaya <tech_nine0400@yahoo.co.jp>
2022-01-20 23:03:17 +0900

    新規登録マイグレーション設定

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,5 +1,4 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,23 +12,25 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->integer('id')->primary(); //プライマリキー
-            $table->string('last_name',255); // 名前（名）
-            $table->string('first_name',255); //名前(性)
-            $table->string('last_name_kana',255); //フリガナ(名)
-            $table->string('fist_name_kana',255); //フリガナ（性）
-            $table->string('full_name',255)->index();  //indexキー、フルネーム
-            $table->integer('post_code'); //郵便番号
-            $table->string('address',255); //住所
-            $table->string('phone_number',255)->unique();//電話番号、ユニークキー
-            $table->string('email')->unique();//Eメール、ユニークキー
-            $table->string('password'); //パスワード
-            $table->boolean('is_deleted'); //退会フラグ
-            $table->timestamps();
-            // $table->rememberToken();　⇦↓のコードは初期のやつで入らなそうなら削除する
-            // $table->timestamp('email_verified_at')->nullable();
-        });
+        if (!Schema::hasTable('users')){
+            Schema::create('users', function (Blueprint $table) {
+                $table->integer('id')->primary(); //プライマリキー
+                $table->string('last_name', 255); // 名前（名）
+                $table->string('first_name', 255); //名前(性)
+                $table->string('last_name_kana', 255); //フリガナ(名)
+                $table->string('fist_name_kana', 255); //フリガナ（性）
+                $table->string('full_name', 255)->index();  //indexキー、フルネーム
+                $table->integer('post_code'); //郵便番号
+                $table->string('address', 255); //住所
+                $table->string('phone_number', 255)->unique();//電話番号、ユニークキー
+                $table->string('email')->unique();//Eメール、ユニークキー
+                $table->string('password'); //パスワード
+                $table->boolean('is_deleted'); //退会フラグ
+                $table->timestamps();
+                // $table->rememberToken();　⇦↓のコードは初期のやつで入らなそうなら削除する
+                // $table->timestamp('email_verified_at')->nullable();
+            });
+        }
     }
 
     /**
