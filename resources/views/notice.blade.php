4178836a0e9e843f06a55eb7533d2413a3a728f1
Hikaru Shiho <shiho97797@gmail.com>
2022-01-21 19:24:27 +0900

    志保 20220121

--- resources/views/notice.blade.php
+++ resources/views/notice.blade.php
@@ -1,176 +1,156 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-        <script src="{{ asset('js/app.js') }}" defer></script>
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        <link href="{{ asset('css/content.css') }}" rel="stylesheet">
-        <!-- Styles -->
-    </head>
-    <body class="antialiased">
+@extends('layouts.app')
+@section('content')
+    <div class="mw-100 py-5 bg-a68a72 text-center">
+        <h2 class="text-white font-weight-bold">お知らせ</h2>
+    </div>
 
 
-
-        <div class="mw-100 py-5 bg-a68a72 text-center">
-            <h2 class="text-white font-weight-bold">お知らせ</h2>
-        </div>
-
-
-        <section class="mw-100">
-            <div class="container py-4 py-md-5">
-                <div class="pt-4">
-                    <h2 class="text-a68a72 font-weight-bold py-6">お知らせ一覧</h2>
-                </div>
-                <ul class="list-unstyled pt-md-4 pt-3">
-                    <li class="border-bottom border-e4d3c4">
-                        <a href="">
-                            <dl class="row mt-0 mb-0 pt-1">
-                                <dt class="col-lg-2 col-12">
-                                    <span class="">2021年12月31日</span>
-                                </dt>
-                                <dd class="col-lg-10 col-12 position-relative arrow_685647_16">
-                                    <span class="font-weight-bold">新商品が入荷いたしました！！</span>
-                                </dd>
-                            </dl>
+    <section class="mw-100">
+        <div class="container py-4 py-md-5">
+            <div class="pt-4">
+                <h2 class="text-a68a72 font-weight-bold py-6">お知らせ一覧</h2>
+            </div>
+            <ul class="list-unstyled pt-md-4 pt-3">
+                <li class="border-bottom border-e4d3c4">
+                    <a href="">
+                        <dl class="row mt-0 mb-0 pt-1">
+                            <dt class="col-lg-2 col-12">
+                                <span class="">2021年12月31日</span>
+                            </dt>
+                            <dd class="col-lg-10 col-12 position-relative arrow_685647_16">
+                                <span class="font-weight-bold">新商品が入荷いたしました！！</span>
+                            </dd>
+                        </dl>
+                    </a>
+                </li>
+                <li class="border-bottom border-e4d3c4">
+                    <a href="">
+                        <dl class="row mt-0 mb-0 mt-4">
+                            <dt class="col-lg-2 col-12">
+                                <span>2021年12月31日</span>
+                            </dt>
+                            <dd class="col-lg-10 col-12 pr-5 position-relative arrow_685647_16 dot_omit">
+                                <span class="font-weight-bold">新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！</span>
+                            </dd>
+                        </dl>
+                    </a>
+                </li>
+                <li class="border-bottom border-e4d3c4">
+                    <a href="">
+                        <dl class="row mt-0 mb-0 mt-4">
+                            <dt class="col-lg-2 col-12">
+                                <span>2021年12月31日</span>
+                            </dt>
+                            <dd class="col-lg-10 col-12 pr-5 position-relative arrow_685647_16 dot_omit">
+                                <span class="font-weight-bold">新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！</span>
+                            </dd>
+                        </dl>
+                    </a>
+                </li>
+                <li class="border-bottom border-e4d3c4">
+                    <a href="">
+                        <dl class="row mt-0 mb-0 mt-4">
+                            <dt class="col-lg-2 col-12">
+                                <span>2021年12月31日</span>
+                            </dt>
+                            <dd class="col-lg-10 col-12 pr-5 position-relative arrow_685647_16 dot_omit">
+                                <span class="font-weight-bold">新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！</span>
+                            </dd>
+                        </dl>
+                    </a>
+                </li>
+                <li class="border-bottom border-e4d3c4">
+                    <a href="">
+                        <dl class="row mt-0 mb-0 mt-4">
+                            <dt class="col-lg-2 col-12">
+                                <span>2021年12月31日</span>
+                            </dt>
+                            <dd class="col-lg-10 col-12 pr-5 position-relative arrow_685647_16 dot_omit">
+                                <span class="font-weight-bold">新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！</span>
+                            </dd>
+                        </dl>
+                    </a>
+                </li>
+                <li class="border-bottom border-e4d3c4">
+                    <a href="">
+                        <dl class="row mt-0 mb-0 mt-4">
+                            <dt class="col-lg-2 col-12">
+                                <span>2021年12月31日</span>
+                            </dt>
+                            <dd class="col-lg-10 col-12 pr-5 position-relative arrow_685647_16 dot_omit">
+                                <span class="font-weight-bold">新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！</span>
+                            </dd>
+                        </dl>
+                    </a>
+                </li>
+                <li class="border-bottom border-e4d3c4">
+                    <a href="">
+                        <dl class="row mt-0 mb-0 mt-4">
+                            <dt class="col-lg-2 col-12">
+                                <span>2021年12月31日</span>
+                            </dt>
+                            <dd class="col-lg-10 col-12 pr-5 position-relative arrow_685647_16 dot_omit">
+                                <span class="font-weight-bold">新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！</span>
+                            </dd>
+                        </dl>
+                    </a>
+                </li>
+                <li class="border-bottom border-e4d3c4">
+                    <a href="">
+                        <dl class="row mt-0 mb-0 mt-4">
+                            <dt class="col-lg-2 col-12">
+                                <span>2021年12月31日</span>
+                            </dt>
+                            <dd class="col-lg-10 col-12 pr-5 position-relative arrow_685647_16 dot_omit">
+                                <span class="font-weight-bold">新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！</span>
+                            </dd>
+                        </dl>
+                    </a>
+                </li>
+                <li class="border-bottom border-e4d3c4">
+                    <a href="">
+                        <dl class="row mt-0 mb-0 mt-4">
+                            <dt class="col-lg-2 col-12">
+                                <span>2021年12月31日</span>
+                            </dt>
+                            <dd class="col-lg-10 col-12 pr-5 position-relative arrow_685647_16 dot_omit">
+                                <span class="font-weight-bold">新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！</span>
+                            </dd>
+                        </dl>
+                    </a>
+                </li>
+                <li class="border-bottom border-e4d3c4">
+                    <a href="">
+                        <dl class="row mt-0 mb-0 mt-4">
+                            <dt class="col-lg-2 col-12">
+                                <span>2021年12月31日</span>
+                            </dt>
+                            <dd class="col-lg-10 col-12 pr-5 position-relative arrow_685647_16 dot_omit">
+                                <span class="font-weight-bold">新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！</span>
+                            </dd>
+                        </dl>
+                    </a>
+                </li>
+            </ul>
+            <nav aria-label="Page navigation example">
+                <ul class="pagination justify-content-center pt-md-5 pt-4">
+                    <li class="page-item">
+                        <a class="page-link" href="#" aria-label="Previous">
+                            <span aria-hidden="true">&laquo;</span>
+                            <span class="sr-only">Previous</span>
                         </a>
                     </li>
-                    <li class="border-bottom border-e4d3c4">
-                        <a href="">
-                            <dl class="row mt-0 mb-0 mt-4">
-                                <dt class="col-lg-2 col-12">
-                                    <span>2021年12月31日</span>
-                                </dt>
-                                <dd class="col-lg-10 col-12 pr-5 position-relative arrow_685647_16 dot_omit">
-                                    <span class="font-weight-bold">新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！</span>
-                                </dd>
-                            </dl>
-                        </a>
-                    </li>
-                    <li class="border-bottom border-e4d3c4">
-                        <a href="">
-                            <dl class="row mt-0 mb-0 mt-4">
-                                <dt class="col-lg-2 col-12">
-                                    <span>2021年12月31日</span>
-                                </dt>
-                                <dd class="col-lg-10 col-12 pr-5 position-relative arrow_685647_16 dot_omit">
-                                    <span class="font-weight-bold">新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！</span>
-                                </dd>
-                            </dl>
-                        </a>
-                    </li>
-                    <li class="border-bottom border-e4d3c4">
-                        <a href="">
-                            <dl class="row mt-0 mb-0 mt-4">
-                                <dt class="col-lg-2 col-12">
-                                    <span>2021年12月31日</span>
-                                </dt>
-                                <dd class="col-lg-10 col-12 pr-5 position-relative arrow_685647_16 dot_omit">
-                                    <span class="font-weight-bold">新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！</span>
-                                </dd>
-                            </dl>
-                        </a>
-                    </li>
-                    <li class="border-bottom border-e4d3c4">
-                        <a href="">
-                            <dl class="row mt-0 mb-0 mt-4">
-                                <dt class="col-lg-2 col-12">
-                                    <span>2021年12月31日</span>
-                                </dt>
-                                <dd class="col-lg-10 col-12 pr-5 position-relative arrow_685647_16 dot_omit">
-                                    <span class="font-weight-bold">新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！</span>
-                                </dd>
-                            </dl>
-                        </a>
-                    </li>
-                    <li class="border-bottom border-e4d3c4">
-                        <a href="">
-                            <dl class="row mt-0 mb-0 mt-4">
-                                <dt class="col-lg-2 col-12">
-                                    <span>2021年12月31日</span>
-                                </dt>
-                                <dd class="col-lg-10 col-12 pr-5 position-relative arrow_685647_16 dot_omit">
-                                    <span class="font-weight-bold">新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！</span>
-                                </dd>
-                            </dl>
-                        </a>
-                    </li>
-                    <li class="border-bottom border-e4d3c4">
-                        <a href="">
-                            <dl class="row mt-0 mb-0 mt-4">
-                                <dt class="col-lg-2 col-12">
-                                    <span>2021年12月31日</span>
-                                </dt>
-                                <dd class="col-lg-10 col-12 pr-5 position-relative arrow_685647_16 dot_omit">
-                                    <span class="font-weight-bold">新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！</span>
-                                </dd>
-                            </dl>
-                        </a>
-                    </li>
-                    <li class="border-bottom border-e4d3c4">
-                        <a href="">
-                            <dl class="row mt-0 mb-0 mt-4">
-                                <dt class="col-lg-2 col-12">
-                                    <span>2021年12月31日</span>
-                                </dt>
-                                <dd class="col-lg-10 col-12 pr-5 position-relative arrow_685647_16 dot_omit">
-                                    <span class="font-weight-bold">新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！</span>
-                                </dd>
-                            </dl>
-                        </a>
-                    </li>
-                    <li class="border-bottom border-e4d3c4">
-                        <a href="">
-                            <dl class="row mt-0 mb-0 mt-4">
-                                <dt class="col-lg-2 col-12">
-                                    <span>2021年12月31日</span>
-                                </dt>
-                                <dd class="col-lg-10 col-12 pr-5 position-relative arrow_685647_16 dot_omit">
-                                    <span class="font-weight-bold">新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！</span>
-                                </dd>
-                            </dl>
-                        </a>
-                    </li>
-                    <li class="border-bottom border-e4d3c4">
-                        <a href="">
-                            <dl class="row mt-0 mb-0 mt-4">
-                                <dt class="col-lg-2 col-12">
-                                    <span>2021年12月31日</span>
-                                </dt>
-                                <dd class="col-lg-10 col-12 pr-5 position-relative arrow_685647_16 dot_omit">
-                                    <span class="font-weight-bold">新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！新商品が入荷いたしました！！</span>
-                                </dd>
-                            </dl>
+                    <li class="page-item"><a class="page-link" href="#">1</a></li>
+                    <li class="page-item"><a class="page-link" href="#">2</a></li>
+                    <li class="page-item"><a class="page-link" href="#">3</a></li>
+                    <li class="page-item">
+                        <a class="page-link" href="#" aria-label="Next">
+                            <span aria-hidden="true">&raquo;</span>
+                            <span class="sr-only">Next</span>
                         </a>
                     </li>
                 </ul>
-                <nav aria-label="Page navigation example">
-                    <ul class="pagination justify-content-center pt-md-5 pt-4">
-                        <li class="page-item">
-                            <a class="page-link" href="#" aria-label="Previous">
-                                <span aria-hidden="true">&laquo;</span>
-                                <span class="sr-only">Previous</span>
-                            </a>
-                        </li>
-                        <li class="page-item"><a class="page-link" href="#">1</a></li>
-                        <li class="page-item"><a class="page-link" href="#">2</a></li>
-                        <li class="page-item"><a class="page-link" href="#">3</a></li>
-                        <li class="page-item">
-                            <a class="page-link" href="#" aria-label="Next">
-                                <span aria-hidden="true">&raquo;</span>
-                                <span class="sr-only">Next</span>
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-        </section>
-
-
-    </body>
-</html>
+            </nav>
+        </div>
+    </section>
+@endsection
